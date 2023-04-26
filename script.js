30dad8be8f8ac53750c614c9718ce8c1d240ae4e
Aniket <t.aniket561@gmail.com>
2023-04-26 22:00:10 +0530

    make functionality to get computer's and user's selection

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 /* 
 
-
+1) get a random choice from the computer
+2) get a choice from the user
+3) rock beats scissor; scissor beats paper; paper beats rock
+4) determine if the player lost or won
 
 */
 
-console.log("Hello");
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+let computerSelection;
+
+if(randomNumber === 1){
+    computerSelection = "rock";
+}
+if(randomNumber === 2){
+    computerSelection = "paper";
+}
+if(randomNumber === 3){
+    computerSelection = "scissors";
+}
+
+let userSelection = prompt("Enter your move [rock, paper or scissors]").toLowerCase();
+
+while(userSelection !== "rock" && userSelection !== "paper" && userSelection !== "scissors"){
+    userSelection = prompt("Please enter you choice again [rock, paper or scissors]")
+}
