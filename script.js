391e5d207ac31bb146f0d15fdb9f80e50910bf71
Aniket <t.aniket561@gmail.com>
2023-05-30 12:34:36 +0530

    make basic functioning ui version of the game

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-/* 
-
-1) get a random choice from the computer
-2) get a choice from the user
-3) rock beats scissor; scissor beats paper; paper beats rock
-4) determine if the player lost or won
-
-*/
-
 function userWins(computerSelection, userSelection){
-
     //returns true if user wins else false
     if((computerSelection === "rock" && userSelection === "scissors") || (computerSelection === "paper" && userSelection == "rock") || (computerSelection === "scissors" && userSelection === "paper")){
         return false;
     }
-
     else if((computerSelection === "rock" && userSelection === "paper") || (computerSelection === "paper" && userSelection === "scissors") || (computerSelection === "scissors" && userSelection === "rock")){
         return true
     }
@@ -21,7 +10,6 @@ function userWins(computerSelection, userSelection){
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-
     // 1 is rock; 2 is paper and 3 is scissors
     if(randomNumber === 1){
         return "rock"
@@ -34,45 +22,53 @@ function getComputerChoice() {
     }
 }
 
-function game(){
-    let userWinCount = 0;
-    let computerWinCount = 0;
-    for(let i = 0; i < 5; i++){
-        
+let userWinCount = 0;
+let computerWinCount = 0;
+
+const userScore = document.querySelector('#user_score');
+const computerScore = document.querySelector('#computer_score')
+
+const selection = document.createElement('p');
+const winner = document.createElement('p');
+
+const buttons = document.querySelectorAll('.buttons>button');
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+
+        //remove the winner text content if there is any after starting a new game
+        winner.textContent = '';
+
+        const userSelection = this.id;
         const computerSelection = getComputerChoice();
-        const userSelection = prompt("Enter your move [rock, paper or scissors]").toLowerCase();
-    
-        //if choice is invalid, ask again
-        while(userSelection !== "rock" && userSelection !== "paper" && userSelection !== "scissors"){
-            userSelection = prompt("Please enter you choice again [rock, paper or scissors]")
-        }
 
         if(userSelection === computerSelection){
-            console.log("It's a tie");
-            continue;
+            selection.textContent = "It's a tie."
         }
         else if(userWins(computerSelection, userSelection)){
             userWinCount++;
-            console.log(`Computer chose ${computerSelection}, you won`);
+            selection.textContent = `Computer chose ${computerSelection}, you won.`
         }
         else{
             computerWinCount++;
-            console.log(`Computer chose ${computerSelection}, you lost`);
+            selection.textContent = `Computer chose ${computerSelection}, you lost`;
         }
 
-        console.log(`Current score: You: ${userWinCount}, Computer: ${computerWinCount}`);
-    }
-    
-    if(userWinCount > computerWinCount){
-        console.log(`Congratulations! You won ${userWinCount} matches out of 5. You win!`);
-    }
-    else if(computerWinCount > userWinCount) {
-        console.log(`Sorry... the computer won ${computerWinCount} matches out of 5. You lose.`);
-    }
-    else{
-        console.log("Nobody wins! It's a tie!");
-    }
+        //show to the user what the computer selected and if they won or lost
+        document.querySelector('.selections').appendChild(selection);
 
-}
+        //display the score
+        userScore.innerHTML = `<b>Your score: </b>${userWinCount}`;
+        computerScore.innerHTML = `<b>Computer's score: </b>${computerWinCount}`;
 
-game()
+        //first to 5 points wins
+        if(userWinCount === 5 || computerWinCount === 5){
+            
+            winner.textContent = userWinCount === 5 ? "You won 5 matches! You won the game!!" : "Computer won 5 matches! You lost the game.";
+            
+            //reset the counts and show to user who won
+            userWinCount = 0;
+            computerWinCount = 0;
+            document.querySelector('.results').appendChild(winner);
+        }
+    })
+})
